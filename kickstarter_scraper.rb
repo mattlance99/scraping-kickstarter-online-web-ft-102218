26b41baa2fbc233126f5fde0089fdb4f7d4eabe8
Matthew Cohen <matt.cohen182@gmail.com>
2018-11-16 18:25:47 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,29 @@
 # require libraries/modules here
+require "nokogiri"
+require "pry"
+
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+ 
+  kickstarter = Nokogiri::HTML(html)
+  
+  projects = {}
+
+
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+
+
+projects[title] = {}
+projects[title][:image_link] = project.css(".projectphoto-little").attr("src").value
+#
+    #:image_link => ,
+    #:description => ,
+    #:location => 
+    #:percent_funded => 
+  #},
+
+  end 
 end
+
